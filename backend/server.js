a83c4fa9942ede73fb52801c7c71346264ae372f
unknown <tezzinthecity@gmail.com>
2021-06-07 23:07:11 +0200

    create Schema, create get and post endpoint for resources

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import listEndpoints from 'express-list-endpoints'
 
 dotenv.config()
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalProject"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.set('useCreateIndex', true); //added due to deprecation error 26868
 mongoose.Promise = Promise
@@ -16,6 +16,49 @@ mongoose.Promise = Promise
 
 const port = process.env.PORT || 9000
 const app = express()
+
+const resourceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The message field is mandatory"],
+    minlength: 2,
+    maxlength: 140,
+  },
+  language: String,
+  type: String,
+  free: Boolean,
+  online: Boolean,
+  description: String,
+  url: String
+});
+
+const Resource = mongoose.model("Resource", resourceSchema);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/resources", async (req, res) => {
+  const allResources = await Resource.find()
+    .sort({ name: 1 })
+    .limit(20)
+    .exec();
+  res.json(allResources);
+});
+
+app.post("/resources", async (req, res) => {
+  try {
+    const newResource = await new Resource(req.body).save();
+    res.json(newResource);
+  } catch (error) {
+    if (error.code === 11000) {
+      res
+        .status(400)
+        .json({ error: "Duplicated value", fields: error.keyValue });
+    }
+    res.status(400).json(error);
+  }
+});
+
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())

5d37485792477c98872e12c9ade72c0104cfc4b4
unknown <tezzinthecity@gmail.com>
2021-07-26 00:00:47 +0200

    add url to reusable, update fetch in communities component

--- frontend/src/pages/Communities.js
+++ frontend/src/pages/Communities.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from "react";
 import {Infobox, Infotext, Cardbox, ButtonLink} from "styled-components/pagesStyles";
 
 import ResourceCard from "components/ResourceCard";
+import { CATEGORY_URL } from "reusable/urls";
 
 
 export const Communities = () => {
   const [resources, setResources] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "https://final-project-wit-app.herokuapp.com/resources/type/community"
-    )
+    fetch(CATEGORY_URL("community"))
       .then((response) => response.json())
       .then((json) => setResources(json));
   }, []);
--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,3 +1,7 @@
 const BASE_URL = 'https://final-project-wit-app.herokuapp.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
+
+const BASIC_URL = 'https://final-project-wit-app.herokuapp.com/resources/type'
+
+export const CATEGORY_URL = (slug) => `${BASIC_URL}/${slug}`
